536223e23a464f0ca593bb2c0eb1e93d921f3660
josemachava <jose.s.machava@gmail.com>
2018-02-13 23:16:06 +0200

    show upload photos

--- photos/urls.py
+++ photos/urls.py
@@ -11,12 +11,15 @@ Class-based views
     2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+    2. Add a U
+    RL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url,include
+from django.urls import path, include
 from django.contrib import admin
+from . import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^photo/', include('photo.urls'))
+    path('', views.index, name="index"),
+    path('admin/', admin.site.urls),
+    path('photo/', include('photo.urls'))
 ]
